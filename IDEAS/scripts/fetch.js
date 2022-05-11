4485843dcd20cd39115a81a4e85ebb5f67baed0a
Lucas Quipildor Portillo <quipildor.lucas@gmail.com>
2022-05-10 21:53:53 -0300

    Proyecto Ticket avanzado

--- IDEAS/scripts/fetch.js
+++
@@ -1,4 +0,0 @@
-const valorDolar=document.querySelector("#valorDolar")
-fetch("https://api-dolar-argentina.herokuapp.com/api/dolaroficial")
-.then(resp=>resp.json())
-.then(data=>valorDolar.innerHTML=data.venta)
