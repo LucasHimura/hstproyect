b5a033953b1700020cb3b3c42436e95b2bd706be
Lucas Quipildor Portillo <quipildor.lucas@gmail.com>
2022-05-26 23:22:03 -0300

    se agrego sweet alert y botones de filtro

---
+++ script/cargar.js
@@ -0,0 +1,58 @@
+
+
+  
+  
+  
+  
+  //Creacion del objeto Dispositivo luego se guarda en un array llamado Equipos 
+  class Dispositivo {
+    constructor(id, nombre, detalle, sistema, prioridad, observacion) {
+      this.id = id
+      this.nombre = nombre;
+      this.detalle = detalle;
+      this.sistema = sistema;
+      this.prioridad = prioridad;
+      this.observacion = observacion;
+  
+  
+    }
+  }
+
+
+let formDispositivo = document.querySelector('#formDispositivo')
+  // genera el numero de incidente para seguimiento
+  function idUnico() {
+    return parseInt(Math.random() * 9999999999)
+  }
+
+  
+  
+  //Recupera la info del Storage para ser mostrada en seccion Incidentes
+  function equipoStorage() {
+    return JSON.parse(localStorage.getItem("ticket")) || [];
+  
+  }
+
+
+  formDispositivo.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let equipos = equipoStorage()
+    let nombre = document.querySelector("#idNombre").value
+    let detalle = document.querySelector("#idDetalle").value
+    let sistema = document.querySelector("#idSistema").value
+    let prioridad = document.querySelector("#idPrioridad").value
+    let observacion = document.querySelector("#idObservacion").value
+    const equipo = new Dispositivo(idUnico(), nombre, detalle, sistema, prioridad, observacion)
+    equipos.push(equipo)
+    //console.log(equipos)
+    formDispositivo.reset()
+    localStorage.setItem("ticket", JSON.stringify(equipos));
+    Swal.fire({
+        title: 'El ticket se ha creado con EXITO!',
+        text: 'En la seccion incidente podras verlo',
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
+})
+
+  
+  })
--- script/script.js
+++ script/script.js
@@ -1,67 +1,25 @@
-// genera el numero de incidente para seguimiento
-function idUnico() {
-  return parseInt(Math.random() * 9999999999)
-}
-
-
-
-
-//Creacion del objeto Dispositivo luego se guarda en un array llamado Equipos 
-class Dispositivo {
-  constructor(id, nombre, detalle, sistema, prioridad, observacion) {
-    this.id = id
-    this.nombre = nombre;
-    this.detalle = detalle;
-    this.sistema = sistema;
-    this.prioridad = prioridad;
-    this.observacion = observacion;
-
-
-  }
-}
-
-
-let equipos = []
-let formDispositivo = document.querySelector('#formDispositivo')
-let botonIncidente = document.querySelector('#botonIncidente')
+const botonIncidente = document.querySelector('#botonIncidente')
 let divIncidente = document.querySelector('#divIncidente')
 let idObservacion = document.querySelector("#idObservacion")
-
-
-
-
-
-
-formDispositivo.addEventListener('submit', (event) => {
-  event.preventDefault()
-  let nombre = document.querySelector("#idNombre").value
-  let detalle = document.querySelector("#idDetalle").value
-  let sistema = document.querySelector("#idSistema").value
-  let prioridad = document.querySelector("#idPrioridad").value
-  let observacion = document.querySelector("#idObservacion").value
-  const equipo = new Dispositivo(idUnico(), nombre, detalle, sistema, prioridad, observacion)
-  equipos.push(equipo)
-  //console.log(equipos)
-  formDispositivo.reset()
-  localStorage.setItem("nombre", JSON.stringify(equipos));
-
-})
-
-
-//Recupera la info del Storage para ser mostrada en seccion Incidentes
-function equipoStorage() {
-  return JSON.parse(localStorage.getItem("nombre")) || [];
+const botonAlta = document.querySelector('#alta')
+const botonUrgente = document.querySelector("#urgente")
+const botonMedia = document.querySelector("#media")
+const botonBaja = document.querySelector("#baja")
+ //Recupera la info del Storage para ser mostrada en seccion Incidentes
+ 
+ function equipoStorage() {
+  return JSON.parse(localStorage.getItem("ticket")) || [];
 
 }
 
 //Muestra en tarjetas la informacion guardada en Storage
-function mostrarEquipos() {
-  const equipos = equipoStorage();
+function mostrarEquipos(array) {
+  
   divIncidente.innerHTML = "";
-  equipos.forEach((elemento) => {
+  array.forEach((elemento) => {
     divIncidente.innerHTML += `
-    <div id= "color" class="col mb-5">
-    <div class="card h-100">
+    <div id= "color" class="col mb-5 ${elemento.prioridad}">
+    <div class="card h-100 ${elemento.prioridad}">
         <div class="card-body p-4">
             <div class="text-center">
             <h5 class="card-title">Incidente:${elemento.id}</h5>
@@ -71,7 +29,7 @@ function mostrarEquipos() {
                 <h5 id= "cambioColor" class= "changeColor" >Prioridad:${elemento.prioridad}</h5>
                 <p>Observacion de Falla:${elemento.observacion}</p>   
                 <div class="d-flex justify-content-around">
-                <button onclick="eliminarIncidente(${elemento.id})" class="">Realizado</button>
+                <button  class=""onclick="eliminarIncidente(${elemento.id})">Realizado</button>
                 
                  
             </div>
@@ -86,49 +44,48 @@ function mostrarEquipos() {
   });
 }
 
-//ELimina el incidente individual una vez que se aprieta el boton realizado
-function eliminarIncidente(id) {
-  let incidentes = equipoStorage()
-  let newIncidentes = incidentes.filter(e => e.id != id)
-  localStorage.setItem("nombre", JSON.stringify(newIncidentes))
-  mostrarEquipos();
-
-}
-/*//Filtra la incidencia por prioridad en este caso Urgente 
-function buscarPrioridad() {
-  let incidentes = equipoStorage()
-  let newIncidentes=incidentes.filter(e=>e.prioridad == "URGENTE")
-  localStorage.setItem("urgentes",JSON.stringify(newIncidentes)) 
-  mostrarEquipos();*/
-
-
-function buscarPrioridad() {
-  let incidentes = equipoStorage()
-  let newUrgencia = incidentes.filter(e => e.prioridad == "URGENTE")
-  localStorage.setItem("urgentes",JSON.stringify(newUrgencia))
-  divIncidente.innerHTML = "";
-  newUrgencia.forEach((elemento) => {
-    divIncidente.innerHTML += `
-      <div id= "color" class="col mb-5">
-      <div class="card h-100">
-          <div class="card-body p-4">
-              <div class="text-center">
-              <h5 class="card-title">Incidente:${elemento.id}</h5>
-                  <h5>Nombre:${elemento.nombre}</h5>
-                  <h5>Detalle:${elemento.detalle}</h5>
-                  <h5>Sistema:${elemento.sistema}</h5>
-                  <h5 id= "cambioColor" class= "changeColor" >Prioridad:${elemento.prioridad}</h5>
-                  <p>Observacion de Falla:${elemento.observacion}</p>   
-                  <div class="d-flex justify-content-around">
-                  <button onclick="eliminarIncidente(${elemento.id})" class="">Realizado</button>
-                </div>
-            </div>
-       </div>
-      </div>
-      `;
-  });
+  //ELimina el incidente individual una vez que se aprieta el boton realizado
+  function eliminarIncidente(id) {
+    let incidentes = equipoStorage()
+    let newIncidentes = incidentes.filter(e => e.id != id)
+    localStorage.setItem("ticket", JSON.stringify(newIncidentes))
+    mostrarEquipos(equipoStorage())
+   
   
-}
+  }
+
+
+botonIncidente.addEventListener("click",()=>{
+  let equipos= equipoStorage()
+  mostrarEquipos(equipos)
+  
+})
+
+botonAlta.addEventListener("click",()=>{
+  let equipos=equipoStorage()
+  let alta =equipos.filter(equipo=>equipo.prioridad == "ALTA")
+  mostrarEquipos(alta)
+})
+botonUrgente.addEventListener("click",()=>{
+  let equipos=equipoStorage()
+  let urgente =equipos.filter(equipo=>equipo.prioridad == "URGENTE")
+  mostrarEquipos(urgente)
+})
+botonMedia.addEventListener("click",()=>{
+  let equipos=equipoStorage()
+  let media =equipos.filter(equipo=>equipo.prioridad == "MEDIA")
+  mostrarEquipos(media)
+})
+botonBaja.addEventListener("click",()=>{
+  let equipos=equipoStorage()
+  let baja =equipos.filter(equipo=>equipo.prioridad == "BAJA")
+  mostrarEquipos(baja)
+})
+
+
+
+
+  
 
 
 
@@ -141,11 +98,10 @@ function buscarPrioridad() {
 
 
 
+//
 
 
 
 
 
 
-
-/*<h3>Nro. Ticket: ${idUnico()}</h3>*/
